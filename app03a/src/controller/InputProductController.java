7734f877f84dfa0ad08b8547b5e9855383a21fd7
caizhaoke <caizhaoke@idwzx.com>
2017-03-24 16:09:42 +0800

    20170324-c2：添加业务控制器以及jsp页面

--- app03a/src/controller/InputProductController.java
+++ app03a/src/controller/InputProductController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -10,8 +12,11 @@ import javax.servlet.http.HttpServletResponse;
  * Created by zhaoke on 2017/3/24.
  */
 public class InputProductController implements Controller {
+    private static final Log logger = LogFactory.getLog(InputProductController.class);
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        logger.info("InputProductController called");
+
         return new ModelAndView("/WEB-INF/jsp/ProductForm.jsp");
     }
 }
--- app03a/src/controller/SaveProductController.java
+++ app03a/src/controller/SaveProductController.java
@@ -1,17 +1,39 @@
 package controller;
 
+import domain.Product;
+import domain.ProductForm;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 
 /**
  * Created by zhaoke on 2017/3/24.
  */
 public class SaveProductController implements Controller {
+    private static final Log logger = LogFactory.getLog(SaveProductController.class);
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        return null;
+        logger.info("SaveProductController called");
+
+        ProductForm productForm =  new ProductForm();
+        productForm.setName(httpServletRequest.getParameter("name"));
+        productForm.setDescription(httpServletRequest.getParameter("description"));
+        productForm.setPrice(httpServletRequest.getParameter("price"));
+
+        Product product = new Product();
+        product.setName(productForm.getName());
+        product.setDescription(productForm.getDescription());
+        try {
+            product.setPrice(new BigDecimal(productForm.getPrice()));
+        }catch (Exception e){
+            throw new IllegalArgumentException("产品价格金额格式不正确");
+        }
+
+        return new ModelAndView("/WEB-INF/jsp/ProductDetails.jsp","product",product);
     }
 }
